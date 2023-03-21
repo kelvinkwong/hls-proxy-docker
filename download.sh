2f8e07fc59b0bd6caf5f073685f826da9b2200f8
Fernando Ruiz <fernando@openwebinars.net>
2023-03-21 09:34:08 +0100

    Add playlist parameter

--- download.sh
+++ download.sh
@@ -5,8 +5,10 @@ ID=$1
 DIR=${OUTDIR:-"/var/hls"}
 MINUTES=${2:-10}
 OPTIONS=${3:-'-d'}
+PLAYLIST=${4:-"https://openwebinars.net/academia/hls/$ID.m3u8"}
 TIMEOUT=$(echo  $MINUTES*60 | bc)
-pipenv run -- ./hlsproxy.py $OPTIONS -o "$DIR/${ID::2}/$ID" "https://openwebinars.net/academia/hls/$ID.m3u8" &
+[ -d "$DIR/${ID::2}/$ID" ] && find "$DIR/${ID::2}/$ID" -type f -size 21c -exec rm {} \;
+pipenv run -- ./hlsproxy.py $OPTIONS -o "$DIR/${ID::2}/$ID" $PLAYLIST &
 echo "Process $! will be killed after $TIMEOUT seconds"
 sleep $TIMEOUT && kill -15 $!
 echo "Sended SIGTERM to $!"
